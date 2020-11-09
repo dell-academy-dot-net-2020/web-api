0897ad434dd17b3f023994904d8d94dd354e4282
Cleo da Silva <cleo.je@gmail.com>
2020-11-09 09:13:52 -0300

    molds criadas

--- Dell.Academy/src/Dell.Academy.Domain/Models/Category.cs
+++ Dell.Academy/src/Dell.Academy.Domain/Models/Category.cs
@@ -3,7 +3,11 @@
     public class Category : BaseEntity
     {
         public string Name { get; private set; }
+      
+
 
         public Category(string name) => Name = name;
+      
+
     }
 }
---
+++ Dell.Academy/src/Dell.Academy.Domain/Models/Provider.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Dell.Academy.Domain.Models
+{
+    public class Provider : BaseEntity
+    {
+
+        public string Name { get; private set; }
+        public string DocumentNumber { get; private set; }
+        public string ProviderType { get; private set; }
+        public bool Active { get; private set; }
+        public Address Address { get; private set; }
+
+        public List<Product> Products { get; private set; }
+
+        public Provider(string name, string documentNumber, string providerType, bool active, Address address, List<Product> products)
+        {
+            Name = name;
+            DocumentNumber = documentNumber;
+            ProviderType = providerType;
+            Active = active;
+            Address = address;
+            Products = products;
+        }
+
+    }
+}
