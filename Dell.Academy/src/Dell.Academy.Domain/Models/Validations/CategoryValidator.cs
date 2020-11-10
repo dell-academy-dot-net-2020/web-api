4d735dbe4fb51d4ab2eeae9ab75ced426ef066e1
regisbr13 <regislima1391@gmail.com>
2020-11-10 10:10:23 -0300

    Iniciando validacoes

---
+++ Dell.Academy/src/Dell.Academy.Domain/Models/Validations/CategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Dell.Academy.Domain.Models.Validations
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .Length(3, 15);
+        }
+    }
+}
---
+++ Dell.Academy/src/Dell.Academy.Domain/Models/Validations/ProviderValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dell.Academy.Domain.Models.Validations
+{
+    public class ProviderValidator : AbstractValidator<Provider>
+    {
+        public ProviderValidator()
+        {
+            RuleFor(p => p.Name);
+        }
+    }
+}
