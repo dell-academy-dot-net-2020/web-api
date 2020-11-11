fc91aeff58ea4d3f332d69340f1920fd64389fc5
regisbr13 <regislima1391@gmail.com>
2020-11-11 08:51:57 -0300

    Adicionando validacoes do commit anterior

---
+++ Dell.Academy/src/Dell.Adacemy.Domain/Models/Validations/ProductValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Dell.Academy.Domain.Models.Validations
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(pd => pd.Name).NotEmpty().Length(3, 15);
+
+            RuleFor(pd => pd.Description).NotEmpty().Length(3, 15);
+
+            RuleFor(pd => pd.Value).NotEmpty();
+
+            RuleFor(pd => pd.Register).NotEmpty();
+
+            RuleFor(pd => pd.Active).NotEmpty();
+
+            RuleFor(pd => pd.Provider).NotEmpty();
+
+            RuleFor(pd => pd.Category).NotEmpty();
+        }
+    }
+}
