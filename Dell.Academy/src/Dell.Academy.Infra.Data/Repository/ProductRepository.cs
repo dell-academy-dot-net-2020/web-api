05dcfc9c738e7bc2ef51fb23fa980704f57fc2c1
Cleo da Silva <cleo.je@gmail.com>
2020-11-19 21:37:41 -0300

    Repository

---
+++ Dell.Academy/src/Dell.Academy.Infra.Data/Repository/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dell.Academy.Domain.Interfaces;
+using Dell.Academy.Domain.Models;
+using Dell.Academy.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Dell.Academy.Infra.Data.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApiContext context) : base(context)
+        {
+        }
+
+        public async Task<Product> GetProductByProviderIdAsync(long providerId)
+            => await DbSet.AsNoTracking().FirstOrDefaultAsync(a => a.ProviderId == providerId);
+    }
+}
---
+++ Dell.Academy/src/Dell.Academy.Infra.Data/Repository/ProviderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Dell.Academy.Domain.Interfaces;
+using Dell.Academy.Domain.Models;
+using Dell.Academy.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Dell.Academy.Infra.Data.Repository
+{
+    public class ProviderRepository : Repository<Provider>, IProviderRepository
+    {
+        public ProviderRepository(ApiContext context) : base(context)
+        {
+        }
+
+        public async Task<Provider> GetProviderByProviderIdAsync(long providerId)
+            => await DbSet.AsNoTracking().FirstOrDefaultAsync(a => a.ProviderId == providerId);
+    }
+}
