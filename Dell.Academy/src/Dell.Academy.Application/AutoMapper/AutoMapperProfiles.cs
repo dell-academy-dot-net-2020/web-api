929e2d3f433bff023e90fdd24303cf7495e4d46d
regisbr13 <regislima1391@gmail.com>
2020-11-24 09:23:39 -0300

    Comecando a implementacao do automapper

---
+++ Dell.Academy/src/Dell.Academy.Application/AutoMapper/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Dell.Academy.Application.ViewModels;
+using Dell.Academy.Domain.Models;
+
+namespace Dell.Academy.Application.AutoMapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Address, AddressViewModel>().ReverseMap();
+            CreateMap<Product, ProductViewModel>()
+                .ForMember(dest => dest.ProviderName, opt => opt.MapFrom(src => src.Provider.Name))
+                .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+        }
+    }
+}
---
+++ Dell.Academy/src/Dell.Academy.Application/ViewModels/ProviderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Dell.Academy.Domain.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Dell.Academy.Application.ViewModels
+{
+    public class ProviderViewModel
+    {
+        public string Name { get; set; }
+        public string DocumentNumber { get; set; }
+        public short ProviderType { get; set; }
+        public DateTime Register { get; set; }
+        public bool Active { get; set; }
+        public Address Address { get; set; }
+        public List<Product> Products { get; private set; }
+    }
+}
