60acb3c698fe596080a87630541ffcf1d8557301
regisbr13 <regislima1391@gmail.com>
2020-12-23 08:44:25 -0300

    Retornando em ordem alfabética

--- Dell.Academy/src/Dell.Academy.Infra.Data/Repository/ProviderRepository.cs
+++ Dell.Academy/src/Dell.Academy.Infra.Data/Repository/ProviderRepository.cs
@@ -3,6 +3,7 @@ using Dell.Academy.Domain.Models;
 using Dell.Academy.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Dell.Academy.Infra.Data.Repository
@@ -17,7 +18,7 @@ namespace Dell.Academy.Infra.Data.Repository
             => await DbSet.AsNoTracking().Include(p => p.Address).Include(p => p.Products).FirstOrDefaultAsync(p => p.Id == id);
 
         public async Task<List<Provider>> GetProvidersWithAddressAsync()
-            => await DbSet.AsNoTracking().Include(p => p.Address).ToListAsync();
+            => await DbSet.AsNoTracking().Include(p => p.Address).OrderBy(p => p.Name).ToListAsync();
 
         public async Task<bool> ProviderWithDocumentNumberExistsAsync(string documentNumber)
             => await DbSet.AsNoTracking().AnyAsync(p => p.DocumentNumber == documentNumber);
