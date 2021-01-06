7f1ce83761ae9ff3b32e29ab63742dc0a183c586
Cleo Silva <cleo.je@hotmail.com>
2021-01-06 09:03:01 -0300

    entrega 1 atividade teste

--- Dell.Academy/tests/Dell.Academy.Domain.Tests/ErrorMessagesTests.cs
+++ Dell.Academy/tests/Dell.Academy.Domain.Tests/ErrorMessagesTests.cs
@@ -20,6 +20,97 @@ namespace Dell.Academy.Domain.Tests
         }
 
         [Fact]
+        public void CategoryNameExistsError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "Categoria já cadastrada.";
+
+            // Act
+            var result = ErrorMessages.CategoryNameExistsError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void ProductSkuExistsError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "Sku do produto já cadastrado.";
+
+            // Act
+            var result = ErrorMessages.ProductSkuExistsError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void IdDoNotMatch_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "Os Id's não correspondem.";
+
+            // Act
+            var result = ErrorMessages.IdDoNotMatch;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void CpfSizeError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "O campo Cpf precisa ter 11 caracteres.";
+
+            // Act
+            var result = ErrorMessages.CpfSizeError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void CpfInvalidError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "O Cpf fornecido não é válido.";
+
+            // Act
+            var result = ErrorMessages.CpfInvalidError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void CnpjSizeError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "O campo Cnpj precisa ter 14 caracteres.";
+
+            // Act
+            var result = ErrorMessages.CnpjSizeError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void CnpjInvalidError_ShouldReturnTheCommitErrorMessage()
+        {
+            // Arrange
+            var errorMessage = "O Cnpj fornecido não é válido.";
+
+            // Act
+            var result = ErrorMessages.CnpjInvalidError;
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
         public void InternalServerError_ReceiveAnExceptionMessage_ShouldReturnAnInternalServerErrorMessage()
         {
             // Arrange
@@ -28,6 +119,48 @@ namespace Dell.Academy.Domain.Tests
 
             // Act
             var result = ErrorMessages.InternalServerError(exceptionMessage);
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void ProviderExistsError_ReceiveAnExceptionMessage_ShouldReturnAnProviderExistsErrorMessage()
+        {
+            // Arrange
+            var exceptionMessage = "Exception message test";
+            var errorMessage = $"Fornecedor com o documento {exceptionMessage}";
+
+            // Act
+            var result = ErrorMessages.ProviderExistsError(exceptionMessage);
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void IntegrityReferenceError_ReceiveAnExceptionMessage_ShouldReturnAnIntegrityReferenceErrorMessage()
+        {
+            // Arrange
+            var exceptionMessage = "Exception message test";
+            var errorMessage = $"Não foi possível exluir: {exceptionMessage}";
+
+            // Act
+            var result = ErrorMessages.IntegrityReferenceError(exceptionMessage);
+
+            // Assert
+            Assert.Equal(errorMessage, result);
+        }
+
+        [Fact]
+        public void NotFoundError_ReceiveAnExceptionMessage_ShouldReturnAnNotFoundErrorMessage()
+        {
+            // Arrange
+            var exceptionMessage = "Exception message test";
+            var errorMessage = $"não foi encontrado(a): {exceptionMessage}";
+
+            // Act
+            var result = ErrorMessages.NotFoundError(exceptionMessage);
 
             // Assert
             Assert.Equal(errorMessage, result);
