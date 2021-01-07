ac5f28ce0169c9558425244e3f234c7beb1e8fc5
regisbr13 <regislima1391@gmail.com>
2021-01-07 11:41:20 -0300

    Testes de unidade implementados na classe CategoryValidator

--- Dell.Academy/tests/Dell.Academy.Domain.Tests/DocsValidationTests.cs
+++ Dell.Academy/tests/Dell.Academy.Domain.Tests/DocsValidationTests.cs
@@ -1,10 +1,16 @@
-﻿using Dell.Academy.Domain.Models.Validations.Utils;
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using Dell.Academy.Domain.Models.Validations.Utils;
 using Xunit;
 
 namespace Dell.Academy.Domain.Tests
 {
     public class DocsValidationTests
     {
+        private readonly Faker _faker;
+
+        public DocsValidationTests() => _faker = new Faker("pt_BR");
+
         [Fact]
         public void CpfValidation_ReceiveACpfNumberWithDiferentSize_ShouldReturnFalse()
         {
@@ -35,16 +41,16 @@ namespace Dell.Academy.Domain.Tests
         public void CpfValidation_ReceiveAValidCpfNumber_ShouldReturnTrue()
         {
             // Arrange
-            var cpfs = new string[] { "12345678901", "15698569874", "11155544400" };
+            var cpf = _faker.Person.Cpf();
 
             // Act
-            var result = CpfValidation.Validate(cpfs[0]) || CpfValidation.Validate(cpfs[1]) || CpfValidation.Validate(cpfs[2]);
+            var result = CpfValidation.Validate(cpf);
 
             // Assert
             Assert.True(result);
         }
 
-       // CNPJ
+        // CNPJ
 
         [Fact]
         public void CnpjValidation_ReceiveACnpjNumberWithDiferentSize_ShouldReturnFalse()
@@ -76,10 +82,10 @@ namespace Dell.Academy.Domain.Tests
         public void CnpjValidation_ReceiveAValidCpfNumber_ShouldReturnTrue()
         {
             // Arrange
-            var cnpjs = new string[] { "1234567890123", "15698569874321", "11155544400066" };
+            var cnpj = _faker.Company.Cnpj();
 
             // Act
-            var result = CnpjValidation.Validate(cnpjs[0]) || CnpjValidation.Validate(cnpjs[1]) || CnpjValidation.Validate(cnpjs[2]);
+            var result = CnpjValidation.Validate(cnpj);
 
             // Assert
             Assert.True(result);
