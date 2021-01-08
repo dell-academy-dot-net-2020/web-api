5d9b32d6f7c983d5715c3452381bc401acb00213
Cleo Silva <cleo.je@hotmail.com>
2021-01-08 08:57:08 -0300

    teste incopleto address e prov

---
+++ Dell.Academy/tests/Dell.Academy.Domain.Tests/AddressValidatorTests.cs
@@ -0,0 +1,371 @@
+﻿using Bogus;
+using Dell.Academy.Domain.Models;
+using Dell.Academy.Domain.Models.Validations;
+using System.Linq;
+using Xunit;
+
+namespace Dell.Academy.Domain.Tests
+{
+    public class AddressValidatorTests
+    {
+        private readonly Faker _faker;
+        private readonly AddressValidator _validator;
+
+        public AddressValidatorTests()
+        {
+            _faker = new Faker("pt_BR");
+            _validator = new AddressValidator();
+        }
+        ///
+        /////////////////////////////////////STREET//////////////////////////////////////////////////////////////////
+        ///
+        [Fact]
+        public void ReceiveAValidAddressStreet_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressStreet_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Rua", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeAddressStreet_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address("C1");
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Rua", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeAddressStreet_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Rua", result.Errors.FirstOrDefault().PropertyName);
+        }
+        ///
+        /////////////////////////////////////NUMBER//////////////////////////////////////////////////////////////////
+        ///
+        [Fact]
+        public void ReceiveAValidAddressNumber_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+       
+        [Fact]
+        public void ReceiveABiggerSizeAddressNumber_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Número", result.Errors.FirstOrDefault().PropertyName);
+        }
+        ///
+        /////////////////////////////////////COMPLEMET//////////////////////////////////////////////////////////////////
+        ///
+
+        [Fact]
+        public void ReceiveAValidAddressComplement_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressComplement_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Complemento", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeAddressComplement_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address("C1");
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Complemento", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeAddressComplement_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(16));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Rua", result.Errors.FirstOrDefault().PropertyName);
+        }
+        ///
+        /////////////////////////////////////CEP//////////////////////////////////////////////////////////////////
+        ///
+
+        [Fact]
+        public void ReceiveAValidAddressCep_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressCep_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("cep", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+      
+
+        [Fact]
+        public void ReceiveABiggerSizeAddressCep_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(9));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Cep", result.Errors.FirstOrDefault().PropertyName);
+        }
+        ///
+        /////////////////////////////////////DISTRICT//////////////////////////////////////////////////////////////////
+        ///
+        [Fact]
+        public void ReceiveAValidAddressDistrict_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressDistrict_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Bairro", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeAddressDistrict_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address("C1");
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Bairro", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeAddressDistrict_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Bairro", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        ///
+        /////////////////////////////////////CITY//////////////////////////////////////////////////////////////////
+        ///
+        [Fact]
+        public void ReceiveAValidAddressCity_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressCity_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Cidade", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeAddressCity_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address("C1");
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Cidade", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeAddressCity_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Cidade", result.Errors.FirstOrDefault().PropertyName);
+        }
+        ///
+        /////////////////////////////////////STATE//////////////////////////////////////////////////////////////////
+        ///
+        [Fact]
+        public void ReceiveAValidAddressState_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyAddressState_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address(string.Empty);
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Estado", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeAddressState_ShouldValidateAddress()
+        {
+            // Arrange
+            var address = new Address("C1");
+
+            // Act
+            var result = _validator.Validate(address);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Estado", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        
+    }
+}
---
+++ Dell.Academy/tests/Dell.Academy.Domain.Tests/ProductValidatorTests.cs
@@ -0,0 +1,259 @@
+﻿using Bogus;
+using Dell.Academy.Domain.Models;
+using Dell.Academy.Domain.Models.Validations;
+using System.Linq;
+using Xunit;
+
+namespace Dell.Academy.Domain.Tests
+{
+    public class ProductValidatorTests
+    {
+        private readonly Faker _faker;
+        private readonly ProductValidator _validator;
+
+        public ProductValidatorTests()
+        {
+            _faker = new Faker("pt_BR");
+            _validator = new ProductValidator();
+        }
+        /// <summary>
+        /// //////////////////////////////////////////NAME////////////////////////////////////////////////////
+        /// </summary>
+        [Fact]
+        public void ReceiveAValidProductName_ShouldValidateProduct()
+        {
+            // Arrange
+            var product = new Product(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(product);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyProductName_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(string.Empty);
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeProductName_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product("C1");
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeProductName_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////DESCRIPITION////////////////////////////////////////////////////
+        /// </summary>
+        [Fact]
+        public void ReceiveAValidProductDescription_ShouldValidateProduct()
+        {
+            // Arrange
+            var product = new Product(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(product);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyProductDescription_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(string.Empty);
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Descrição", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeProductDescription_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product("C1");
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Descrição", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeProductDescription_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(_faker.Random.AlphaNumeric(51));
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Descrição", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////SKU////////////////////////////////////////////////////
+        /// </summary>
+        [Fact]
+        public void ReceiveAValidProductSku_ShouldValidateProduct()
+        {
+            // Arrange
+            var product = new Product(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(product);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyProductSku_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(string.Empty);
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("",result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeProductSku_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product("C1");
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Descrição", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////VALUE////////////////////////////////////////////////////
+        /// </summary>
+
+        [Fact]
+        public void ReceiveAValidProductValue_ShouldValidateProduct()
+        {
+            // Arrange
+            var product = new Product(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(product);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ReceiveAnEmptyProductValue_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(string.Empty);
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Valor", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeProductValue_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product("C1");
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Valor", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////REGISTER////////////////////////////////////////////////////
+        /// </summary>
+        /// 
+
+        [Fact]
+        public void ReceiveAValidProductRegister_ShouldValidateProduct()
+        {
+            // Arrange
+            var product = new Product(_faker.Commerce.Categories(1).FirstOrDefault());
+
+            // Act
+            var result = _validator.Validate(product);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        
+
+        [Fact]
+        public void ReceiveABiggerSizeProductRegister_ShouldValidateProduct()
+        {
+            // Arrange
+            var Product = new Product(_faker.Random.AlphaNumeric(31));
+
+            // Act
+            var result = _validator.Validate(Product);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Registro", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+    }
+}
