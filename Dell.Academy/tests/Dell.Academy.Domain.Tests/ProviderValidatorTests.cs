4ac354cc1ee4b67c9f461c84dfad70dcb758aeb0
Cleo Silva <cleo.je@hotmail.com>
2021-01-13 08:33:52 -0300

    provide part

--- Dell.Academy/tests/Dell.Academy.Domain.Tests/ProviderValidatorTests.cs
+++ Dell.Academy/tests/Dell.Academy.Domain.Tests/ProviderValidatorTests.cs
@@ -4,6 +4,8 @@ using Dell.Academy.Domain.Models;
 using Dell.Academy.Domain.Models.Enums;
 using Dell.Academy.Domain.Models.Validations;
 using Xunit;
+using System.Linq; // adcionado
+
 
 namespace Dell.Academy.Domain.Tests
 {
@@ -44,5 +46,58 @@ namespace Dell.Academy.Domain.Tests
             // Assert
             Assert.True(result.IsValid);
         }
+
+        /// <summary>
+        /// //////////////////////////////////////////NAMEcnpj////////////////////////////////////////////////////
+        /// </summary>
+        
+        [Fact]
+        public void ReceiveAnEmptyProviderName_ShouldValidateProvider()
+        {
+           
+            // Arrange
+            var provider = new Provider(string.Empty, _faker.Company.Cnpj(), ProviderType.Company, true, null, null);
+
+            // Act
+            var result = _validator.Validate(provider);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveASmallerSizeProviderName_ShouldValidateProvider()
+        {
+            // Arrange
+             var provider = new Provider("P1", _faker.Company.Cnpj(), ProviderType.Company, true, null, null);
+
+            // Act
+            var result = _validator.Validate(provider);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        [Fact]
+        public void ReceiveABiggerSizeProviderName_ShouldValidateProvider()
+        {
+            // Arrange
+            var provider = new Provider(_faker.Random.AlphaNumeric(31), _faker.Company.Cnpj(), ProviderType.Company, true, null, null);
+           
+            // Act
+            var result = _validator.Validate(provider);
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal("Nome", result.Errors.FirstOrDefault().PropertyName);
+        }
+
+        /// <summary>
+        /// //////////////////////////////////////////cnpj////////////////////////////////////////////////////
+        /// </summary>
+
+        
     }
 }
